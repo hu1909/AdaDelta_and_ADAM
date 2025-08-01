d3e347cc1a010963b791c6d47801b91aa2712089
Quoc Hung NGUYEN <quochung.nguyen@upskills.com>
2025-08-01 12:02:27 +0700

    Modify the last time

--- implementation/EMA_class.py
+++ implementation/EMA_class.py
@@ -1,45 +1,30 @@
 import numpy as np 
 
 class EMA:
-    def __init__(self, alpha=None, window=None, bias_correction=True):
-
-        if alpha is not None and window is not None:
-            raise ValueError("Specify either alpha or window, not both")
-        
-        if alpha is not None:
-            if not 0 < alpha <= 1:
-                raise ValueError("Alpha must be between 0 and 1")
-            self.alpha = alpha
-        elif window is not None:
-            if window <= 0:
-                raise ValueError("Window must be positive")
-            self.alpha = 2.0 / (window + 1)
-        else:
-            raise ValueError("Must specify either alpha or window")
-        
+    def __init__(self, beta=None, bias_correction=True):
+        self.beta = beta
         self.bias_correction = bias_correction
         self.t = 0
-        self.ema_value = None
+        self.ema = None
 
     
     
     def calculate(self, new_value):
         self.t += 1
-        if self.ema_value is None:
-            self.ema_value = new_value
+        if self.ema is None:
+            self.ema= new_value
         else:
-            self.ema_value = self.alpha * new_value + (1 - self.alpha) * self.ema_value
+            self.ema = self.beta * new_value + (1 - self.beta) * self.ema
         
         if self.bias_correction:
-            bias_corrected_ema = self.ema_value / (1 - (1 - self.alpha) ** self.t)
-            return bias_corrected_ema
+            bias_ema = self.ema / (1 - (1 - self.beta) ** self.t)
+            return bias_ema
         else:
             return self.ema_value
     
     def get_current_value(self):
-        if self.ema_value is None:
-            return None
         if self.bias_correction:
-            return self.ema_value / (1 - (1 - self.alpha) ** self.t)
+            return self.ema / (1 - (1 - self.beta) ** self.t)
         else:
-            return self.ema_value
+            return self.ema
+        
